f7557d19496a186c436cb83bdb538e0d9c48f252
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-26 01:13:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,3 @@
-#def nyc_pigeon_organizer(data)
-  # write your code here!
-  #return_data = {}
-  #new_data = {}
-  #array_of_names = []
-  #new_data=data[:color]
-  #new_data.each do |key, value|
-    #test_array = value
-    #test_array.each do |index|
-      #array_of_names << index
-      #return_data = Hash[array_of_names.map {|value| [value, [{:colors => nil}]]}]
-    #end
-    #array_of_names << value
-  #end
-  #p return_data
-  #return_data
-#end
-
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
